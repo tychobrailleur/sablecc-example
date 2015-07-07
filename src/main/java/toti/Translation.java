d004f0b3e1e79003bfa7158d4354a89ebccf5114
Sébastien Le Callonnec <sebastien@weblogism.com>
2015-07-07 09:08:24 +0200

    Assignment, and print value.

--- src/main/java/toti/Translation.java
+++ src/main/java/toti/Translation.java
@@ -7,6 +7,7 @@ import toti.node.*;
 class Translation extends DepthFirstAdapter {
 
     private Map identifiers = new HashMap();
+    private Map values = new HashMap();
 
     public void caseAValDeclaration(AValDeclaration node) {
         TIdentifier id = node.getIdentifier();
@@ -31,5 +32,29 @@ class Translation extends DepthFirstAdapter {
         }
 
         identifiers.put(id.getText(), id);
+    }
+
+    public void caseAAssignAssignment(AAssignAssignment node) {
+        TIdentifier id = node.getIdentifier();
+        if (identifiers.get(id.getText()) == null) {
+            throw new RuntimeException("Undefined variable: " + id.getText());
+        }
+
+        TNumber number = node.getNumber();
+        values.put(id.getText(), Integer.parseInt(number.getText()));
+    }
+
+    public void caseAPrintStmt(APrintStmt node) {
+        TIdentifier id = node.getIdentifier();
+        if (identifiers.get(id.getText()) == null) {
+            throw new RuntimeException("Undefined variable: " + id.getText());
+        }
+
+        if (values.get(id.getText()) == null) {
+            throw new RuntimeException("Uninitialized variable: " + id.getText());
+        }
+
+        Integer value = (Integer)values.get(id.getText());
+        System.out.println(value);
     }
 }
